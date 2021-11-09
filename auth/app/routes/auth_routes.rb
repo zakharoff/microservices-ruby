30d2875916db32e8d0984dedd6041c42a0e1ce24
Roman Zakharov <roman.zakharov@jetrockets.pro>
2021-11-09 16:59:21 +0300

    add auth route

--- auth/app/routes/auth_routes.rb
+++ auth/app/routes/auth_routes.rb
@@ -1,7 +1,17 @@
 class AuthRoutes < Application
-  helpers PaginationLinks
+  helpers Auth
 
-  namespace '/v1' do
-   # TBD
+  post '/' do
+    result = Auth::FetchUserService.call(extracted_token['uuid'])
+
+    if result.success?
+      meta = { user_id: result.user.id }
+
+      status 200
+      json meta: meta
+    else
+      status 403
+      error_response(result.errors)
+    end
   end
 end
