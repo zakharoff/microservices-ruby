f463aa167bf04f9dc644ef9c8172e73c509867aa
Roman Zakharov <roman.zakharov@jetrockets.pro>
2021-11-28 18:00:43 +0300

    base log for geocoder

--- geocoder/app/lib/ads_service/async/rpc_client.rb
+++ geocoder/app/lib/ads_service/async/rpc_client.rb
@@ -47,7 +47,7 @@ module AdsService
           @queue.publish(
             payload,
             opts.merge(
-              app_id: 'geocoder',
+              app_id: Settings.app.name,
               correlation_id: @correlation_id,
               reply_to: @reply_queue.name
             )
--- geocoder/config/initializers/consumer.rb
+++ geocoder/config/initializers/consumer.rb
@@ -2,8 +2,16 @@ channel = RabbitMq.consumer_channel
 queue = channel.queue('geocoding', durable: true)
 
 queue.subscribe(manual_ack: true) do |delivery_info, properties, payload|
+  Thread.current[:request_id] = properties.headers['request_id']
+
   payload = JSON(payload)
   coordinates = Geocoder.geocode(payload['city'])
+
+  Application.logger.info(
+    'geocoded coordinates',
+    city: payload['city'],
+    coordinates: coordinates
+  )
 
   if coordinates.any?
     # client = AdsService::Async::RpcClient.fetch
